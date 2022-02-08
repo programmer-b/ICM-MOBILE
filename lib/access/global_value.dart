dbaa59819bf4d12ca518cf3f1afbb49ccd433888
dante <dancan219w@gmail.com>
2022-02-08 13:40:25 +0300

    01#commit

--- lib/access/global_value.dart
+++ lib/access/global_value.dart
@@ -8,6 +8,18 @@ class GlobalString extends StatelessWidget{
     // TODO: implement build
     throw UnimplementedError();
   }
-  static String usernameError = "Username cannot be blank";
-  static String passwordError = "Password cannot be empty";
+  static String usernameError = "";
+  static String passwordError = "";
+  static String token = "";
+  static String username = "";
+}
+
+class GlobalBool extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+  static bool hasPasswordError = false;
+  static bool hasUsernameError = false;
 }
